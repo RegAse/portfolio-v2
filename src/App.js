d774d50805276bbae3c2654562758326e68d97d3
RegAse <gudmundur210@gmail.com>
2022-10-05 15:27:57 +0000

    Added the basic layout and finished the main billboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="wrap">
+      {/* <img src="/images/svg-5.svg" className="background-glow" alt="Failed loading..." /> */}
+        <div className="container profile-container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="profile-box">
+                <img src="/images/svg-1.svg" className="lines-animation" alt="Failed loading..." />                
+                <img className="img-fluid profile-image" src="./images/me-2.png" />
+                <h2 className="py-4">Guðmundur Axel Guðrúnarson</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="divider-1">
+
+      </div>
+      <div className="wrap-2">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 text-center">
+              <h3>Web Projects</h3>
+              <p className="text-grey">Summary of my knowledge with the web</p>
+            </div>
+            <div className="col-sm-6 text-center showcase-container">
+              <h3>Task App</h3>
+              <img className="img-fluid showcase-image" src="./images/task-app.png" />
+              <p className="text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div className="col-sm-6 text-center showcase-container">
+              <h3>Todo App</h3>
+              <img className="img-fluid showcase-image" src="./images/countdown-app.png" />
+              <p className="text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div className="col-sm-6 text-center showcase-container">
+              <h3>Task App</h3>
+              <img className="img-fluid showcase-image" src="./images/task-app.png" />
+              <p className="text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div className="col-sm-6 text-center showcase-container">
+              <h3>Todo App</h3>
+              <img className="img-fluid showcase-image" src="./images/countdown-app.png" />
+              <p className="text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div className="col-sm-6 text-center showcase-container">
+              <h3>Task App</h3>
+              <img className="img-fluid showcase-image" src="./images/task-app.png" />
+              <p className="text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div className="col-sm-6 text-center showcase-container">
+              <h3>Todo App</h3>
+              <img className="img-fluid showcase-image" src="./images/countdown-app.png" />
+              <p className="text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12 text-center">
+              <h3>Video Game Projects</h3>
+              <p className="text-grey">Summary of my knowledge with Unity</p>
+            </div>
+            <div className="col-sm-6 text-center showcase-container">
+              <h3>Task App</h3>
+              <img className="img-fluid showcase-image" src="./images/task-app.png" />
+              <p className="text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div className="col-sm-6 text-center showcase-container">
+              <h3>Todo App</h3>
+              <img className="img-fluid showcase-image" src="./images/countdown-app.png" />
+              <p className="text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+          </div>
+        </div>
+        <svg>
+          <g transform="matrix(0.913966,0.561753,-3.39923,3.43168,725.462,-921.519)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(0.913966,0.561753,-3.31525,3.3469,817.061,-942.984)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(0.913966,0.561753,-2.09692,2.11694,557.904,-609.251)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(0.913966,0.561753,-2.09692,2.11694,575.231,-568.659)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(0.913966,0.561753,-3.39923,3.43168,879.498,-821.069)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(0.913966,0.561753,-3.31525,3.3469,971.096,-842.535)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(0.913966,0.561753,-2.09692,2.11694,711.939,-508.802)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(0.913966,0.561753,-2.09692,2.11694,729.267,-468.209)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(-0.913966,0.561753,3.39923,3.43168,1289.55,-821.069)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(-0.913966,0.561753,3.31525,3.3469,1197.95,-842.535)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(-0.913966,0.561753,2.09692,2.11694,1457.11,-508.802)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(-0.913966,0.561753,2.09692,2.11694,1439.78,-468.209)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(-0.913966,0.561753,3.39923,3.43168,1135.51,-720.62)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(-0.913966,0.561753,3.31525,3.3469,1043.91,-742.085)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(-0.913966,0.561753,2.09692,2.11694,1303.07,-408.352)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+          <g transform="matrix(-0.913966,0.561753,2.09692,2.11694,1285.74,-367.76)">
+            <rect x="220" y="76" width="14" height="390.509" style={{ fill: "red" }} />
+          </g>
+        </svg>
+      </div>
     </div>
   );
 }
