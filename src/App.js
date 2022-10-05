bc97aa12d503099bccca593028435631b62b34db
RegAse <gudmundur210@gmail.com>
2022-10-05 16:07:38 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ function App() {
               <p className="text-grey">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Tasks hello
               </p>
             </div>
             <div className="col-sm-6 text-center showcase-container">
