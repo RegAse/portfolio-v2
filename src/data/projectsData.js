fad8a356c78bd6c7e4e653a3231555cd98e202d7
RegAse <gudmundur210@gmail.com>
2022-11-12 23:27:50 +0000

    Added the music player visualizer project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,6 +2,15 @@ export const data = {
     projects: [
         {
             id: 0,
+            name: "Music Player With Visualizer",
+            description: "A music player with a couple of songs to choose from and watch the visualizer",
+            labels: [{name: "React"}, {name: "Bootstrap"}, {name: "Javascript"}, {name: "CSS"}, {name: "HTML"}],
+            media: "url(./images/web-designer-site-13.gif)",
+            liveLink: "https://github.com/RegAse/music-player-visualizer",
+            githubLink: "https://github.com/RegAse/music-player-visualizer"
+        },
+        {
+            id: 1,
             name: "CodeQuiz",
             description: "A quiz site for programming related questions, with a little special effect showcasing employee names",
             labels: [{name: "React"}, {name: "Bootstrap"}, {name: "Javascript"}, {name: "CSS"}, {name: "HTML"}],
@@ -10,16 +19,16 @@ export const data = {
             githubLink: "https://github.com/RegAse/codequiz"
         },
         {
-            id: 1,
+            id: 2,
             name: "Graphic Designer Landing Page",
             description: "A simple graphic designer landing page",
             labels: [{name: "React"}, {name: "Bootstrap"}, {name: "Javascript"}, {name: "CSS"}, {name: "HTML"}],
             media: "url(./images/web-designer-site-6.gif)",
             liveLink: "https://regase.github.io/designer-landing-page/",
-            githubLink: "https://github.com/RegAse/designer-landing-page"
+            githubLink: "https://github.com/RegAse/detsigner-landing-page"
         },
         {
-            id: 2,
+            id: 3,
             name: "Portfolio Site",
             description: "A portfolio site to showcase my projects and my skill",
             labels: [{name: "React"}, {name: "Bootstrap"}, {name: "Javascript"}, {name: "CSS"}, {name: "HTML"}],
@@ -28,14 +37,15 @@ export const data = {
             githubLink: "https://github.com/RegAse/portfolio-v2"
         },
         {
-            id: 3,
+            id: 4,
             name: "SVG Sandbox",
             description: "A small project, where you can customize premade svg graphics",
             labels: [{name: "React"}, {name: "Bootstrap"}, {name: "Javascript"}, {name: "CSS"}, {name: "HTML"}],
             media: "url(./images/web-designer-site-9.gif)",
             liveLink: "https://regase.github.io/svg-design-app/",
             githubLink: "https://github.com/RegAse/svg-design-app"
-        },
+        }
+        
         // {
         //     id: 3,
         //     name: "Video Game Landing Page",
